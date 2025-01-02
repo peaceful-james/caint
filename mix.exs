ea46aafb188b519dc83493dfde32b8ad1c2c6468
James Gough <james@peacefulprogramming.dev>
2025-01-02 05:46:39 +0000

    configure coveralls

--- mix.exs
+++ mix.exs
@@ -9,7 +9,16 @@ defmodule Caint.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.xml": :test,
+        "coveralls.json": :test
+      ]
     ]
   end
 
