85ebe07116d286b769b1aecbbfc96dab4cf2ccda
James Gough <james@peacefulprogramming.dev>
2025-01-02 04:11:45 +0000

    test percentage module

--- lib/caint/percentage.ex
+++ lib/caint/percentage.ex
@@ -3,12 +3,26 @@ defmodule Caint.Percentage do
   Calculate completion percentages for locales
   """
 
+  @type input_number :: integer | binary() | Decimal.t()
+
+  @doc """
+  Effectively returns "% done"
+
+  e.g. 19/20 = 95% done
+  """
+  @spec percentage(input_number, input_number) :: Decimal.t()
   def percentage(top, bottom) do
     top_dec = Decimal.new(top)
     bottom_dec = Decimal.new(bottom)
     perc(top_dec, bottom_dec)
   end
 
+  @doc """
+  Effectively returns "% left to do"
+
+  e.g. 19/20 = 5% left to do
+  """
+  @spec anti_percentage(input_number, input_number) :: Decimal.t()
   def anti_percentage(top, bottom) do
     top_dec = Decimal.new(top)
     bottom_dec = Decimal.new(bottom)
---
+++ test/caint/percentage_test.exs
@@ -0,0 +1,26 @@
+defmodule Caint.PercentageTest do
+  use ExUnit.Case
+
+  alias Caint.Percentage
+
+  # def percentage(top, bottom) do
+  # def anti_percentage(top, bottom) do
+  #
+
+  describe "percentage/2" do
+    test "returns the percentage of the top number in relation to the bottom number" do
+      assert Percentage.percentage(19, 20) == Decimal.new("95.00")
+      assert Percentage.percentage(1, 2) == Decimal.new("50.00")
+      assert Percentage.percentage(2, 2) == Decimal.new("100.00")
+      assert Percentage.percentage(3, 2) == Decimal.new("150.00")
+    end
+  end
+
+  describe "anti_percentage/2" do
+    test "returns the percentage of the bottom number in relation to the bottom number minus the top number" do
+      assert Percentage.anti_percentage(19, 20) == Decimal.new("5.00")
+      assert Percentage.anti_percentage(20, 20) == Decimal.new("0.00")
+      assert Percentage.anti_percentage(3, 2) == Decimal.new("-50.00")
+    end
+  end
+end
